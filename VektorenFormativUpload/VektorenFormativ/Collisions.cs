104b1f8da38ebe97e5f6847c9acec16dd96e2aa1
DESKTOP-BC3V8EV\Tobias <tobis-stroedicke@gmx.de>
2018-09-19 14:37:43 +0200

    Vektorberechnung überarbeitet Matrixerstellung (Konstruktor) Kollision "Punkt in Kreis" und "Kreis in Kreis"

--- VektorenFormativUpload/VektorenFormativ/Collisions.cs
+++ VektorenFormativUpload/VektorenFormativ/Collisions.cs
@@ -8,10 +8,24 @@
 
         public static bool PointInSphere(Vector _point, Sphere _sphere)
         {
+            Vector vMag = _point - _sphere.m_Center;
+            float fMagSqr = Vector.SqrMagnitude(vMag);
+
+            if (fMagSqr <= (_sphere.m_Radius * _sphere.m_Radius))
+                return true;
+            else
+                return false;
         }
 
         public static bool SphereInSphere(Sphere _sphere1, Sphere _sphere2)
         {
+            Vector vMagn = _sphere1.m_Center - _sphere2.m_Center;
+            float fMagnSqr = Vector.SqrMagnitude(vMagn);
+
+            if (fMagnSqr <= (_sphere1.m_Radius * _sphere1.m_Radius + _sphere2.m_Radius * _sphere2.m_Radius))
+                return true;
+            else
+                return false;
         }
 
         public static bool CuboidInCuboid(Cuboid _quad1, Cuboid _quad2)
